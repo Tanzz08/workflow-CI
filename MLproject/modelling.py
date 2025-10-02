f0d3365ca64b40f9205ca5a4de512e447a5de1b7
Bustanul Khair R <142671543+Tanzz08@users.noreply.github.com>
2025-10-02 19:44:47 +0800

    Update modelling.py

--- MLproject/modelling.py
+++ MLproject/modelling.py
@@ -12,7 +12,11 @@ if __name__ == "__main__":
     np.random.seed(42)
 
     n_estimators = int(sys.argv[1]) if len(sys.argv) > 1 else 200
-    max_depth = int(sys.argv[2]) if len(sys.argv) > 2 else 20
+    max_depth = int(sys.argv[2]) if len(sys.argv) > 2 else 20   
+    min_sample_split = 5
+    min_sample_leaf = 3
+    random_state = 42
+    criterion = 'gini'
 
     # file dataset
     base_path = os.path.dirname(os.path.abspath(__file__))
@@ -21,10 +25,12 @@ if __name__ == "__main__":
     y_train = pd.read_csv(os.path.join(base_path, "y_train.csv"))
     y_test = pd.read_csv(os.path.join(base_path, "y_test.csv"))
 
+    
+
     input_example = X_train[0:5]
 
     with mlflow.start_run() as run:
-        mlflow.autolog()
+        
         model = RandomForestClassifier(
             n_estimators=n_estimators,
             max_depth=max_depth,
@@ -33,22 +39,37 @@ if __name__ == "__main__":
             random_state=42
         )
 
+        mlflow.log_param('n_estimators', n_estimators)
+        mlflow.log_param('max_depth', max_depth)
+        mlflow.log_param('min_sample_split', min_sample_split)
+        mlflow.log_param('min_sample_leaf', min_sample_leaf)
+        mlflow.log_param('random_state', random_state)
+        mlflow.log_param('criterion', criterion)
+        mlflow.log_param('selected_features', important_feature_names)
+
         model.fit(X_train, y_train)
         y_pred = model.predict(X_test)
+
+        # log important features
+        importances = model.feature_importances_
+        important_feature_names = X_train.columns[np.argsort(importances)[::-1][:10]]
+        mlflow.log_param('selected_features', important_feature_names.tolist())
                                
         mlflow.sklearn.log_model(
             sk_model=model,
-            artifact_path='model',
+            artifact_path="model",
             input_example=input_example
         )
-                               
 
+        # log dataset artifact
+        mlflow.log_artifact(os.path.join(base_path, "X_train.csv"))
+        mlflow.log_artifact(os.path.join(base_path, "X_test.csv"))
+        mlflow.log_artifact(os.path.join(base_path, "y_train.csv"))
+        mlflow.log_artifact(os.path.join(base_path, "y_test.csv"))
+                            
         # evaluasi
         mlflow.log_metric("accuracy", accuracy_score(y_test, y_pred))
         mlflow.log_metric("precision", precision_score(y_test, y_pred, average="weighted"))
         mlflow.log_metric("recall", recall_score(y_test, y_pred, average="weighted"))
         mlflow.log_metric("f1_score", f1_score(y_test, y_pred, average="weighted"))
-
-        run_id = run.info.run_id
-        print(f"MLFLOW_RUN_ID={run_id}")
         
