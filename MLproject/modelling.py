c7fa5d3ff49a107bc2c896fea6061e6fc3fa9ecf
Bustanul Khair R <142671543+Tanzz08@users.noreply.github.com>
2025-10-02 20:44:21 +0800

    Update modelling.py

--- MLproject/modelling.py
+++ MLproject/modelling.py
@@ -13,6 +13,10 @@ if __name__ == "__main__":
 
     n_estimators = int(sys.argv[1]) if len(sys.argv) > 1 else 200
     max_depth = int(sys.argv[2]) if len(sys.argv) > 2 else 20   
+    min_sample_split = 5
+    min_sample_leaf = 3
+    random_state = 42
+    criterion = 'gini'
 
     # file dataset
     base_path = os.path.dirname(os.path.abspath(__file__))
@@ -21,6 +25,8 @@ if __name__ == "__main__":
     y_train = pd.read_csv(os.path.join(base_path, "y_train.csv"))
     y_test = pd.read_csv(os.path.join(base_path, "y_test.csv"))
 
+    
+
     input_example = X_train[0:5]
 
     with mlflow.start_run():
@@ -28,10 +34,18 @@ if __name__ == "__main__":
         model = RandomForestClassifier(
             n_estimators=n_estimators,
             max_depth=max_depth,
-            min_samples_leaf=3,
-            min_samples_split=5, 
-            random_state=42
+            min_samples_leaf=min_sample_leaf,
+            min_samples_split=min_sample_split, 
+            random_state=random_state,
+            criterion=criterion
         )
+
+        mlflow.log_param('n_estimators', n_estimators)
+        mlflow.log_param('max_depth', max_depth)
+        mlflow.log_param('min_sample_split', min_sample_split)
+        mlflow.log_param('min_sample_leaf', min_sample_leaf)
+        mlflow.log_param('random_state', random_state)
+        mlflow.log_param('criterion', criterion)
 
         model.fit(X_train, y_train)
         y_pred = model.predict(X_test)
@@ -41,8 +55,11 @@ if __name__ == "__main__":
             sk_model=model,
             artifact_path="model",
             input_example=input_example
-        )
-        # Log metrics
-        accuracy = model.score(X_test, y_test)
-        mlflow.log_metric("accuracy", accuracy)
+        
+                            
+        # evaluasi
+        mlflow.log_metric("accuracy", accuracy_score(y_test, y_pred))
+        mlflow.log_metric("precision", precision_score(y_test, y_pred, average="weighted"))
+        mlflow.log_metric("recall", recall_score(y_test, y_pred, average="weighted"))
+        mlflow.log_metric("f1_score", f1_score(y_test, y_pred, average="weighted"))
         
