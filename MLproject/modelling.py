ff3d6964cdde4ab9adfa45ab92859a3d6667b24b
Bustanul Khair R <142671543+Tanzz08@users.noreply.github.com>
2025-10-02 20:34:04 +0800

    Update modelling.py

--- MLproject/modelling.py
+++ MLproject/modelling.py
@@ -13,10 +13,6 @@ if __name__ == "__main__":
 
     n_estimators = int(sys.argv[1]) if len(sys.argv) > 1 else 200
     max_depth = int(sys.argv[2]) if len(sys.argv) > 2 else 20   
-    min_sample_split = 5
-    min_sample_leaf = 3
-    random_state = 42
-    criterion = 'gini'
 
     # file dataset
     base_path = os.path.dirname(os.path.abspath(__file__))
@@ -24,8 +20,6 @@ if __name__ == "__main__":
     X_test = pd.read_csv(os.path.join(base_path, "X_test.csv"))
     y_train = pd.read_csv(os.path.join(base_path, "y_train.csv"))
     y_test = pd.read_csv(os.path.join(base_path, "y_test.csv"))
-
-    
 
     input_example = X_train[0:5]
 
@@ -39,13 +33,6 @@ if __name__ == "__main__":
             random_state=42
         )
 
-        mlflow.log_param('n_estimators', n_estimators)
-        mlflow.log_param('max_depth', max_depth)
-        mlflow.log_param('min_sample_split', min_sample_split)
-        mlflow.log_param('min_sample_leaf', min_sample_leaf)
-        mlflow.log_param('random_state', random_state)
-        mlflow.log_param('criterion', criterion)
-
         model.fit(X_train, y_train)
         y_pred = model.predict(X_test)
 
@@ -55,11 +42,7 @@ if __name__ == "__main__":
             artifact_path="model",
             input_example=input_example
         )
-        
-                            
-        # evaluasi
-        mlflow.log_metric("accuracy", accuracy_score(y_test, y_pred))
-        mlflow.log_metric("precision", precision_score(y_test, y_pred, average="weighted"))
-        mlflow.log_metric("recall", recall_score(y_test, y_pred, average="weighted"))
-        mlflow.log_metric("f1_score", f1_score(y_test, y_pred, average="weighted"))
+        # Log metrics
+        accuracy = model.score(X_test, y_test)
+        mlflow.log_metric("accuracy", accuracy)
         
