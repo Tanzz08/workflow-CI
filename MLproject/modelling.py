6a50270d5bd803b1f510a0e2d87592eb172d62ff
Bustanul Khair R <142671543+Tanzz08@users.noreply.github.com>
2025-10-03 13:16:30 +0800

    Update modelling.py

--- MLproject/modelling.py
+++ MLproject/modelling.py
@@ -12,11 +12,7 @@ if __name__ == "__main__":
     np.random.seed(42)
 
     n_estimators = int(sys.argv[1]) if len(sys.argv) > 1 else 200
-    max_depth = int(sys.argv[2]) if len(sys.argv) > 2 else 20   
-    min_sample_split = 5
-    min_sample_leaf = 3
-    random_state = 42
-    criterion = 'gini'
+    max_depth = int(sys.argv[2]) if len(sys.argv) > 2 else 20
 
     # file dataset
     base_path = os.path.dirname(os.path.abspath(__file__))
@@ -25,41 +21,34 @@ if __name__ == "__main__":
     y_train = pd.read_csv(os.path.join(base_path, "y_train.csv"))
     y_test = pd.read_csv(os.path.join(base_path, "y_test.csv"))
 
-    
-
     input_example = X_train[0:5]
 
-    with mlflow.start_run():
-        
+    with mlflow.start_run() as run:
+        mlflow.autolog()
         model = RandomForestClassifier(
             n_estimators=n_estimators,
             max_depth=max_depth,
-            min_samples_leaf=min_sample_leaf,
-            min_samples_split=min_sample_split, 
-            random_state=random_state,
-            criterion=criterion
+            min_samples_leaf=3,
+            min_samples_split=5, 
+            random_state=42
         )
-
-        mlflow.log_param('n_estimators', n_estimators)
-        mlflow.log_param('max_depth', max_depth)
-        mlflow.log_param('min_sample_split', min_sample_split)
-        mlflow.log_param('min_sample_leaf', min_sample_leaf)
-        mlflow.log_param('random_state', random_state)
-        mlflow.log_param('criterion', criterion)
 
         model.fit(X_train, y_train)
         y_pred = model.predict(X_test)
-
                                
         mlflow.sklearn.log_model(
             sk_model=model,
-            artifact_path="model",
+            artifact_path='model',
             input_example=input_example
-        
-                            
+        )
+                               
+
         # evaluasi
         mlflow.log_metric("accuracy", accuracy_score(y_test, y_pred))
         mlflow.log_metric("precision", precision_score(y_test, y_pred, average="weighted"))
         mlflow.log_metric("recall", recall_score(y_test, y_pred, average="weighted"))
         mlflow.log_metric("f1_score", f1_score(y_test, y_pred, average="weighted"))
+
+        run_id = run.info.run_id
+        print(f"MLFLOW_RUN_ID={run_id}")
         
